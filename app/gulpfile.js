a98f2e1e97dc6e7205d12d840d017b137dffef50
Kevin Carpenter <kevin@grayboxpdx.com>
2016-01-14 20:51:42 -0800

    Initial rewrapping of structure, wrapped into basic angular / express instance, working saving posting basic blog posts, converting to snippets next. @jon need to fix import paths in main css, not importing NEAT,bourbon, etc.

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -1,20 +1,26 @@
 'use strict';
 
 var gulp = require('gulp'),
+    sass = require('gulp-sass'),
   concat = require('gulp-concat'),
   uglify = require('gulp-uglify'),
-    sass = require('gulp-sass'),
     maps = require('gulp-sourcemaps'),
     jade = require('gulp-jade'),
    watch = require('gulp-watch');
 
+var paths = {
+    scss: 'dist/assets/sass/*.scss',
+    css: 'dist/assets/css/'
+};
+
 gulp.task('styles', function() {
-  gulp.src('dist/assets/sass/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('dist/assets/css/'))
+    //.pipe(gulp.dest('dist/assets/css/'))
+    gulp.src(paths.scss)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(paths.css))
 });
 
 //Watch
 gulp.task('default', function() {
-  gulp.watch('dist/assets/sass/*.scss',['styles']);
+    gulp.watch(paths.scss,['styles']);
 });
