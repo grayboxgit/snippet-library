d23ce71b04c238fdebf0291f0510dc8176af00b0
Jon Haarstad <jon.haarstad@grayboxpdx.com>
2016-01-15 08:56:20 -0800

    Update to gulp mapping and adding in jade compiler to gulp
    - also updated some folder mapping so it will load the right files in the /dist folder

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -1,26 +1,41 @@
 'use strict';
 
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-  concat = require('gulp-concat'),
-  uglify = require('gulp-uglify'),
-    maps = require('gulp-sourcemaps'),
-    jade = require('gulp-jade'),
-   watch = require('gulp-watch');
+var  gulp = require('gulp'),
+     sass = require('gulp-sass'),
+   concat = require('gulp-concat'),
+   uglify = require('gulp-uglify'),
+     maps = require('gulp-sourcemaps'),
+     jade = require('gulp-jade'),
+    watch = require('gulp-watch');
 
 var paths = {
     scss: 'dist/assets/sass/*.scss',
-    css: 'dist/assets/css/'
+     css: 'dist/assets/css/',
+ jadeOrg: 'views/*.jade'
 };
 
 gulp.task('styles', function() {
     //.pipe(gulp.dest('dist/assets/css/'))
     gulp.src(paths.scss)
-        .pipe(sass().on('error', sass.logError))
+        //.pipe(sass().on('error', sass.logError))
+        .pipe(sass({
+          style: 'compressed'
+        }))
         .pipe(gulp.dest(paths.css))
+});
+
+gulp.task('templates', function() {
+    var YOUR_LOCALS = {};
+
+    gulp.src('./views/*.jade')
+      .pipe(jade({
+        locals: YOUR_LOCALS
+      }))
+      .pipe(gulp.dest('./dist/'))
 });
 
 //Watch
 gulp.task('default', function() {
     gulp.watch(paths.scss,['styles']);
+    gulp.watch(paths.jadeOrg,['templates']);
 });
